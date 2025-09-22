e330205ee9317434c0f454e85305e82badb6dcec
JUNHOCHOI0309 <skyho0309@naver.com>
2025-09-22 19:25:05 +0900

    feat : api 기능 추가(소셜 로그인, 아이디, 비밀번호 찾기, 회원가입)

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,16 +1,70 @@
 //로그인 페이지
 import e, { Router } from "express";
 import * as authController from "../controllers/auth.controller.js";
+import { validateRegister } from "../middlewares/auth.middleware.js";
+import * as authService from "../services/auth.service.js";
+import passport from "passport";
 
 const router = Router();
 
-router.post("/register" , authController.register);
+router.post("/register" , validateRegister,authController.register);
 router.post("/login" , authController.login);
 router.post("/logout" , authController.logout);
-router.get("/me" , authController.me); // 현재 로그인한 사용자 정보 조회
 
-router.post("social/:provider", (req,res)=> {res.send("social login: ${req.params.provider}")});
-router.post("/find-id", (req, res)=> { res.send("find id")});
-router.post("/reset-password", (req, res)=> {res.send("reset password")});
+router.get("/me" , authController.me); // 현재 로그인한 사용자 정보 조회
+router.post("/complete-profile" , authController.completeProfile);
+
+router.get("/google", passport.authenticate("google", { scope: ["profile","email"] }));
+router.get("/google/callback", passport.authenticate("google", { failureRedirect: "/login", failureFlash: true }), (req, res) => {
+        //res.json({ message: "Google login success", userId: req.user._id, nickname: req.user.nickname, isProfileComplete: req.user.isProfileComplete });
+        res.redirect("/api/main");
+});
+
+router.get("/kakao", passport.authenticate("kakao"));
+router.get("/kakao/callback", passport.authenticate("kakao", { failureRedirect: "/login", failureFlash: true}), (req, res) => {
+        //res.json({ message: "Kakao login success", userId: req.user._id, nickname: req.user.nickname , isProfileComplete: req.user.isProfileComplete });
+        res.redirect("/api/main");
+});
+
+router.get("/github", passport.authenticate("github", { scope: ["user:email"] }));
+router.get("/github/callback", passport.authenticate("github", { failureRedirect: "/login", failureFlash: true }), (req, res) => {
+        //res.json({ message: "GitHub login success", userId: req.user._id, nickname: req.user.nickname , isProfileComplete: req.user.isProfileComplete });
+        res.redirect("/api/main");
+});
+
+
+router.post("/find-id", async (req, res, next)=> { 
+        try{
+                const { email } = req.body;
+                const id = await authService.findIdByEmail(email);
+                res.json({ message: "User ID found", id });
+        }
+        catch(error){
+                next(error);
+        }      
+});
+router.post("/reset-password", async (req, res, next)=> {
+        try{
+                const { id, email } = req.body;
+                const token = await authService.createPasswordResetToken(id, email);
+
+                res.json({ message: "Password reset email sent if the user exists." });
+        }catch(error){
+                next(error);
+        }
+});
+router.post("/reset-password/:token", async (req, res, next)=> {
+        try{
+                const { token } = req.params;
+                const { newPassword } = req.body;
+                 if (!newPassword || newPassword.length < 6) {
+                        return res.status(400).json({ message: "비밀번호는 최소 6자리 이상이어야 합니다" });
+                }
+                await authService.resetPassword(token, newPassword);
+                res.json({ message: "Password has been reset successfully." });
+        } catch(error){
+                next(error);
+        }
+});
 
 export default router;
