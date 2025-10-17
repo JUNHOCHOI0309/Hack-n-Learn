b0cc1f667587a03fbe65e7ca968384f4ae3e5e0e
JUNHOCHOI0309 <skyho0309@naver.com>
2025-10-18 00:03:05 +0900

    chore : 간단한 오타 수정

--- backend/src/routes/problems.routes.js
+++ backend/src/routes/problems.routes.js
@@ -1,12 +1,12 @@
 // 문제 관련 라우트
 import { Router } from "express";
-import * as problemController from "../controllers/problem.controller.js";
-import requireLogin from "../middlewares/auth.middleware";
+import * as problemController from "../controllers/problems.controller.js";
+import requireLogin from "../middlewares/auth.middleware.js";
 import { validateQuery, validateBody } from "../middlewares/validateQuery.js";
 const router = Router();
 
-router.get("/", requireLogin, validateQuery('getProblems'), problemController.getProblems);
-router.get("/:id", requireLogin, validateQuery('getProblemById'), problemController.getProblemById);
+router.get("/", requireLogin, validateQuery('getProblems'), problemController.listProblems);
+router.get("/:id", requireLogin, validateQuery('getProblemById'), problemController.getProblem);
 router.post("/:id/submit", requireLogin, validateBody('submitFlag'), problemController.submitFlag);
 router.post("/:id/request-hint", requireLogin, validateBody('requestHint'), problemController.requestHint);
 export default router;
