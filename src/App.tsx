d89f221d13d4ca77982d637c5c291764927e57e7
dlakrp123 <dlakrp731@gmail.com>
2025-10-25 19:59:45 +0900

    Feat: 실전문제 메인 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import { Routes, Route } from 'react-router-dom';
 import LearningPageDetail from './pages/LearningPageDetail';
 import LearningPageQuiz from './pages/LearningPageQuiz';
 import LearningPageQuizResult from './pages/LearningPageQuizResult';
+import PracticalProblemsPage from './pages/PracticalProblemsPage';
+import LearningPageMain from './pages/LearningPageMain';
+
+import PracticalProblemsDetailPage from './pages/PracticalProblemsDetailPage';
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<LearningPageDetail />} />
+      <Route path="/" element={<LearningPageMain />} />
+      <Route path="/learning-detail" element={<LearningPageDetail />} />
       <Route path="/learningPageQuiz" element={<LearningPageQuiz />} />
       <Route path="/quiz-results" element={<LearningPageQuizResult />} />
+      <Route path="/practical-problems" element={<PracticalProblemsPage />} />
+      <Route path="/practical-problems/:id" element={<PracticalProblemsDetailPage />} />
     </Routes>
   );
 }
